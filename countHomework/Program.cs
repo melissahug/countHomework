734c0e093abd9bccc998e9fd1e9460dc720621f6
Melissa <hughes.melissa1@gmail.com>
2016-09-09 10:01:08 -0400

    initial commit

---
+++ countHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 1; count <= 99; count += 2)
+
+            {
+                Console.WriteLine(count);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
